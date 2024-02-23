777c3cae833e95e6937b40e1918fd0b84a3569bc
sailormoonvicky <victoriahuhh@gmail.com>
2024-02-23 17:08:06 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import "./App.css"
+
+function App() {
+
+  return (
+    <main>
+      <div className="about-me"></div>
+      <div className="my-projects"></div>
+    </main>
+  )
+}
+
+export default App
